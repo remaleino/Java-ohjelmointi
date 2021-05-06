92639fd995e37a62b1ea8b587e81b12c96a32baa
remaleino <75699175+remaleino@users.noreply.github.com>
2021-05-06 18:43:58 +0300

    Update Vesiputous.java

--- Vesiputous.java
+++ Vesiputous.java
@@ -72,7 +72,6 @@ public class Laatikko {
 					String kortti = kortinValitsin(kortit);
 					kortinTehtava(kortti);
 					kortit.remove(kortti);
-					System.out.print(kortit.size() + " PELIMETODI");
 					siirtoja--;
 				} 
 				else if (ottaaKortin == false || kortit.size() > 1) {
@@ -81,6 +80,7 @@ public class Laatikko {
 				}
 			}
 		} while (siirtoja > 0);
+		System.out.print("Kiitos pelistä!");
 	}
 	
 	public static void lisaaKortit(List<String> kortit) {
@@ -109,13 +109,21 @@ public class Laatikko {
 				array[i] = temp;
 		}
 	}
+	
+	private static String kortinValitsin(List<String> kortit) {
+		Random r=new Random();
+		return kortit.get(r.nextInt(kortit.size()));
+	}
 	private static void kortinTehtava(String kortti) {
 		System.out.println("Korttisi on " + kortti);
 		String[] lista = kortti.split(" ", 2);
 		String arvo = lista[0];
 		switch (arvo) {
 			case "1":
-				System.out.println("Juo 1 tuoppi2!");
+				System.out.println("VESIPUTOUS!");
+				System.out.println("Kaikki pelaajat aloittavat juomaan yhtä aikaa. ");
+				System.out.print("Kortin saaneella pelaajalla on oikeus lopettaa ensin. ");
+				System.out.print("Muut pelaajat voivat lopettaa juomisen vasta, kun vasemmalla seisova pelaaja on lopettanut juomisen.");
 				break;
 			case "2":
 				System.out.println("Juo 2 tuoppia!");
@@ -124,44 +132,50 @@ public class Laatikko {
 				System.out.println("Juo 3 tuoppia!");
 				break;
 			case "4":
-				System.out.println("Juo 4 tuoppia!");
+				System.out.println("Java!");
+				System.out.println("Viimeinen pelaaja joka huuttaa Java juo!");
 				break;
 			case "5":
-				System.out.println("Juo 5 tuoppia!");
+				System.out.println("123!");
+				System.out.println("Kortin nostaja juo yhden hörpyn, seuraava kaksi, ja niin edespäin.");
 				break;
 			case "6":
-				System.out.println("Juo 6 tuoppia!");
+				System.out.println("Kategoria!");
+				System.out.println("Kortin saanut pelaaja valitsee kategorian, ja kaikkien on sanottava vuorotellen siihen kuuluvia asioita.");
 				break;
 			case "7":
-				System.out.println("Juo 7 tuoppia!");
+				System.out.println("Peukalopaini!");
+				System.out.println("Kortin nostaja haasta toiseen pelaajan peukalopainiin. Häviäjä juo 3 hörppyä.");
 				break;
 			case "8":
-				System.out.println("Juo 8 tuoppia!");
+				System.out.println("Sääntö!");
+				System.out.println("Kortin nostaja päättää säännön, jonka kaikkien pelaajien on noudatettava. Muuten joutuu juoman.");
 				break;
 			case "9":
-				System.out.println("Juo 9 tuoppia!");
+				System.out.println("Kysymyspeli!");
+				System.out.println("Aloittaen kortin nostaneesta henkilöstä, pelaajat esittävät toisilleen vuorotellen kysymyksiä. Jos pelaaja ei vasta kysymyksen kysymyksellä, hän joutuu juomaan.");
 				break;
 			case "10":
-				System.out.println("Juo 10 tuoppia!");
+				System.out.println("Tarina!");
+				System.out.println("Keksikkää yhdessä tarina. Jokainen toistaa tarinan ja saa täydentää sitä yhdellä sanalla. Pelaaja joka unohtaa tarinan juo yhden hörpyn.");
 				break;
 			case "Ässä":
-				System.out.println("Juo 13 tuoppia!");
+				System.out.println("Ylös!");
+				System.out.println("Kaikki pelaajat nousevat seisomaan. Viimeinen noussut juo yhden höroyn.");
 				break;
 			case "Jätkä":
-				System.out.println("Juo 11 tuoppia!");
+				System.out.println("Kysymysmestari!");
+				System.out.println("Kortin nostaja on kysymysmestari, jonka kysymyksiin ei saa vastata tai joutuu juomaan yhden hörpyn.");
 				break;
 			case "Kuningas":
-				System.out.println("Juo 12 tuoppia!");
+				System.out.println("Kuningashörppy!");
+				System.out.println("Pelaaja maistelee kaikkien juomia.");
 				break;
 			case "Kuningatar":
-				System.out.println("Juo 13 tuoppia!");
+				System.out.println("Huora!");
+				System.out.println("Pelaaja valitsee itselleen henkilön, joka juo aina hänen juodessa.");
 				break;
 		}
-	}
-	
-	private static String kortinValitsin(List<String> kortit) {
-		Random r=new Random();
-		return kortit.get(r.nextInt(kortit.size()));
 	}
 }
 
