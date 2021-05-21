3d5df00a3d2b18e8cb1c0ba4cd717349d1594f0a
jevgenix <71660541+jevgenix@users.noreply.github.com>
2021-05-21 23:18:05 +0300

    Update Vesiputous.java

--- Vesiputous.java
+++ Vesiputous.java
@@ -1,36 +1,47 @@
 package School;
-
 import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 
 public class Vesiputous {
 
 	public static void Vesiputous() {
-	
-		System.out.print("Tervetuloa Vesiputouspeliin!" + "\nKirjoittakaa pelaajien määrä (2-10): ");		
+
+		System.out.print("Tervetuloa Vesiputouspeliin!" + "\nKirjoittakaa pelaajien määrä (2-10): ");
 		// tätä voidaan pelata vain kaveriporukassa
-		String[] nimet;		
+		String[] nimet;
 		Scanner lukija = new Scanner(System.in);
 		int määrä;
+		boolean loop = true;
 		// kysytään pelaajien määrä
+
+		// ?????
+
+		
 		while (true) {
-			määrä = lukija.nextInt();
-			if (määrä < 2 || määrä > 10) {
-				System.out.print("\nValitettavasti kyseinen lukumäärä ei käy, yritä uudestaan: ");
-				continue;
-			} else {
+			Scanner lukija2 = new Scanner(System.in);
+			try {
+			int aMäärä = lukija2.nextInt();
+			if (aMäärä > 2 && aMäärä < 11) {
+				määrä = aMäärä;
 				break;
 			}
-		}	
-		nimet = new String[määrä];	
-			for (int i = 0; i < määrä; i++) {
-				System.out.print("Anna " + (i + 1) + ". pelaajan nimi: ");
-				nimet[i] = lukija.next();
-			}
 		
+			} catch (Exception ex) {
+			}
+			System.out.print("\nValitettavasti kyseinen lukumäärä ei käy, yritä uudestaan: ");
+				
+			}
+			
+		nimet = new String[määrä];
+		for (int i = 0; i < määrä; i++) {
+			System.out.print("Anna " + (i + 1) + ". pelaajan nimi: ");
+			nimet[i] = lukija.next();
+		}
+
 		// Sekoitetaan pelaajat ja ilmoitetaan järjestys.
 		pelaajienSekoittaminen(nimet);
 		// Luodaan korttien lista.
@@ -63,125 +74,136 @@ public class Vesiputous {
 		} while (siirtoja > 0);
 		System.out.print("Kiitos pelistä!");
 	}
+
 	public static class Vuorot {
 		private int paikka;
 		private String[] lista;
+
 		public Vuorot(int alku, String[] nimet) {
 			this.paikka = alku;
 			this.lista = nimet;
 		}
-		public void seuraava(){
+
+		public void seuraava() {
 			if (this.paikka == (this.lista.length - 1)) {
 				this.paikka = 0;
 			} else {
 				this.paikka += 1;
 			}
 		}
+
 		public String mikaPelaaja() {
 			return this.lista[this.paikka];
 		}
 	}
-	
+
 	public static void pelaajienSekoittaminen(String[] nimet) {
 		Vesiputous.shuffle(nimet);
 		System.out.print("Pelaajien järjestys:\n");
 		for (int i = 0; i < nimet.length; i++) {
-			System.out.print((i + 1) + ". pelaaja " + nimet[i] + "\n");		
+			System.out.print((i + 1) + ". pelaaja " + nimet[i] + "\n");
 		}
 	}
-	
+
 	public static void shuffle(Object[] array) {
-		
+
 		int elements = array.length;
 		for (int i = 0; i < elements; i++) {
-			int s = 1 + (int)(Math.random() * (elements - 1));
-				Object temp = array[s];
-				array[s] = array[i];
-				array[i] = temp;
+			int s = 1 + (int) (Math.random() * (elements - 1));
+			Object temp = array[s];
+			array[s] = array[i];
+			array[i] = temp;
 		}
 	}
-	
+
 	public static void lisaaKortit(List<String> kortit) {
 		int[] paka = new int[52];
-        String[] maa = {"Pata", "Hertta", "Risti", "Ruutu"};
-        String[] arvo = {"Ässä", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jätkä", "Kuningatar", "Kuningas"};
+		String[] maa = { "Pata", "Hertta", "Risti", "Ruutu" };
+		String[] arvo = { "Ässä", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jätkä", "Kuningatar", "Kuningas" };
 
-        for (int i = 0; i < paka.length; i++) {
-            paka[i] = i;
-        }
+		for (int i = 0; i < paka.length; i++) {
+			paka[i] = i;
+		}
 
-        for (int i = 0; i < 52; i++) {
-            String maaK = maa[paka[i] / 13];
-            String arvoK = arvo[paka[i] % 13];
-            kortit.add(arvoK + " " + maaK);
-          }
+		for (int i = 0; i < 52; i++) {
+			String maaK = maa[paka[i] / 13];
+			String arvoK = arvo[paka[i] % 13];
+			kortit.add(arvoK + " " + maaK);
+		}
 	}
-	
+
 	private static String kortinValitsin(List<String> kortit) {
-		Random r=new Random();
+		Random r = new Random();
 		return kortit.get(r.nextInt(kortit.size()));
 	}
+
 	private static void kortinTehtava(String kortti) {
 		System.out.println("Korttisi on " + kortti);
 		String[] lista = kortti.split(" ", 2);
 		String arvo = lista[0];
 		switch (arvo) {
-			case "1":
-				System.out.println("VESIPUTOUS!");
-				System.out.println("Kaikki pelaajat aloittavat juomaan yhtä aikaa. ");
-				System.out.print("Kortin saaneella pelaajalla on oikeus lopettaa ensin. ");
-				System.out.print("Muut pelaajat voivat lopettaa juomisen vasta, kun vasemmalla seisova pelaaja on lopettanut juomisen.");
-				break;
-			case "2":
-				System.out.println("Juo 2 tuoppia!");
-				break;
-			case "3":
-				System.out.println("Juo 3 tuoppia!");
-				break;
-			case "4":
-				System.out.println("Java!");
-				System.out.println("Viimeinen pelaaja joka huuttaa Java juo!");
-				break;
-			case "5":
-				System.out.println("123!");
-				System.out.println("Kortin nostaja juo yhden hörpyn, seuraava kaksi, ja niin edespäin.");
-				break;
-			case "6":
-				System.out.println("Kategoria!");
-				System.out.println("Kortin saanut pelaaja valitsee kategorian, ja kaikkien on sanottava vuorotellen siihen kuuluvia asioita.");
-				break;
-			case "7":
-				System.out.println("Peukalopaini!");
-				System.out.println("Kortin nostaja haasta toiseen pelaajan peukalopainiin. Häviäjä juo 3 hörppyä.");
-				break;
-			case "8":
-				System.out.println("Sääntö!");
-				System.out.println("Kortin nostaja päättää säännön, jonka kaikkien pelaajien on noudatettava. Muuten joutuu juoman.");
-				break;
-			case "9":
-				System.out.println("Kysymyspeli!");
-				System.out.println("Aloittaen kortin nostaneesta henkilöstä, pelaajat esittävät toisilleen vuorotellen kysymyksiä. Jos pelaaja ei vasta kysymyksen kysymyksellä, hän joutuu juomaan.");
-				break;
-			case "10":
-				System.out.println("Tarina!");
-				System.out.println("Keksikkää yhdessä tarina. Jokainen toistaa tarinan ja saa täydentää sitä yhdellä sanalla. Pelaaja joka unohtaa tarinan juo yhden hörpyn.");
-				break;
-			case "Ässä":
-				System.out.println("Ylös!");
-				System.out.println("Kaikki pelaajat nousevat seisomaan. Viimeinen noussut juo yhden höroyn.");
-				break;
-			case "Jätkä":
-				System.out.println("Kysymysmestari!");
-				System.out.println("Kortin nostaja on kysymysmestari, jonka kysymyksiin ei saa vastata tai joutuu juomaan yhden hörpyn.");
-				break;
-			case "Kuningas":
-				System.out.println("Kuningashörppy!");
-				System.out.println("Pelaaja maistelee kaikkien juomia.");
-				break;
-			case "Kuningatar":
-				System.out.println("Huora!");
-				System.out.println("Pelaaja valitsee itselleen henkilön, joka juo aina hänen juodessa.");
-				break;
+		case "1":
+			System.out.println("VESIPUTOUS!");
+			System.out.println("Kaikki pelaajat aloittavat juomaan yhtä aikaa. ");
+			System.out.print("Kortin saaneella pelaajalla on oikeus lopettaa ensin. ");
+			System.out.print(
+					"Muut pelaajat voivat lopettaa juomisen vasta, kun vasemmalla seisova pelaaja on lopettanut juomisen.");
+			break;
+		case "2":
+			System.out.println("Juo 2 tuoppia!");
+			break;
+		case "3":
+			System.out.println("Juo 3 tuoppia!");
+			break;
+		case "4":
+			System.out.println("Java!");
+			System.out.println("Viimeinen pelaaja joka huuttaa Java juo!");
+			break;
+		case "5":
+			System.out.println("123!");
+			System.out.println("Kortin nostaja juo yhden hörpyn, seuraava kaksi, ja niin edespäin.");
+			break;
+		case "6":
+			System.out.println("Kategoria!");
+			System.out.println(
+					"Kortin saanut pelaaja valitsee kategorian, ja kaikkien on sanottava vuorotellen siihen kuuluvia asioita.");
+			break;
+		case "7":
+			System.out.println("Peukalopaini!");
+			System.out.println("Kortin nostaja haasta toiseen pelaajan peukalopainiin. Häviäjä juo 3 hörppyä.");
+			break;
+		case "8":
+			System.out.println("Sääntö!");
+			System.out.println(
+					"Kortin nostaja päättää säännön, jonka kaikkien pelaajien on noudatettava. Muuten joutuu juoman.");
+			break;
+		case "9":
+			System.out.println("Kysymyspeli!");
+			System.out.println(
+					"Aloittaen kortin nostaneesta henkilöstä, pelaajat esittävät toisilleen vuorotellen kysymyksiä. Jos pelaaja ei vasta kysymyksen kysymyksellä, hän joutuu juomaan.");
+			break;
+		case "10":
+			System.out.println("Tarina!");
+			System.out.println(
+					"Keksikkää yhdessä tarina. Jokainen toistaa tarinan ja saa täydentää sitä yhdellä sanalla. Pelaaja joka unohtaa tarinan juo yhden hörpyn.");
+			break;
+		case "Ässä":
+			System.out.println("Ylös!");
+			System.out.println("Kaikki pelaajat nousevat seisomaan. Viimeinen noussut juo yhden höroyn.");
+			break;
+		case "Jätkä":
+			System.out.println("Kysymysmestari!");
+			System.out.println(
+					"Kortin nostaja on kysymysmestari, jonka kysymyksiin ei saa vastata tai joutuu juomaan yhden hörpyn.");
+			break;
+		case "Kuningas":
+			System.out.println("Kuningashörppy!");
+			System.out.println("Pelaaja maistelee kaikkien juomia.");
+			break;
+		case "Kuningatar":
+			System.out.println("Huora!");
+			System.out.println("Pelaaja valitsee itselleen henkilön, joka juo aina hänen juodessa.");
+			break;
 		}
 	}
 }
